2b7e71b2a92c142f84dad445e2a2a33d3feca1ae
Bernardo <berramuspegonzalez@gmail.com>
2026-03-01 23:00:55 -0300

    feat: agregar configuración de URL base y nombre del repositorio en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. La URL base de la cuenta o la organización en GitHub
+  site: 'https://CODES-UNLU.github.io',
+
+  // 2. El nombre del repositorio donde se alojará el sitio
+  base: '/WebE-Sports/',
+
+  // Configuración actual de Tailwind v4
   vite: {
     plugins: [tailwindcss()]
   }
